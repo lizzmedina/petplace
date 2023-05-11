15e74847c45ecd1a79e281e0f079a1880342eea6
lizz medina <lizzmedina15@gmail.com>
2023-05-11 12:15:41 -0500

    navbar - botones y eslogan alineados

--- Front/src/components/NavBar.jsx
+++ Front/src/components/NavBar.jsx
@@ -1,23 +1,23 @@
 import { Link } from 'react-router-dom';
-import '../styles.css';
+import '.././styles.css';
 
 export const NavBar = () => {
   return (
-    <nav className='navbar'>
+    <nav className = 'navbar'>
     
-    <Link to="/"> 
-      <img 
-        src='./images/pet-house.png'  
-        alt="icono"
-        className="logo"
-      /> 
-      <p className='slogan-navbar'> Los alojamos como en casa </p>
-    </Link> 
+      <Link className = 'navbar-link-container' to = "/"> 
+        <img 
+          src='./images/pet-house.png'  
+          alt="icono"
+          className="logo"
+        /> 
+        <p className='slogan-navbar'> Los alojamos como en casa </p>
+      </Link> 
     
-    <div className='navbar-buttons'>
-      <button className='add-acount-button'>Crear cuenta</button>
-      <button className='start-session-button'>Iniciar Sesión</button>
-    </div>
+      <div className ='navbar-buttons'>
+        <button className ='add-acount-button'>Crear cuenta</button>
+        <button className ='start-session-button'>Iniciar Sesión</button>
+      </div>
     
     </nav>
     
