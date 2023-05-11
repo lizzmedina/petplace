3953e00cda0408e6534b11d54592c3e9193e3c15
VivianaGuzmanBuritica <82985780+VivianaGuzmanBuritica@users.noreply.github.com>
2023-05-11 14:24:18 -0500

    Creacion proyecto SpringBoot, creacion dde carpetas, y parte del Modelo de clases, pendientes modelo booking, Pet, y PetDayCare

---
+++ Back/integradorDH/src/main/java/IntegradorDH/demo/model/Booking.java
@@ -0,0 +1,5 @@
+package IntegradorDH.demo.model;
+
+public class Booking {
+    // completar clase: atributos( asegurarse que se importen las clases cuando se quieren colocar como atributos, constructor y metodos )
+}
