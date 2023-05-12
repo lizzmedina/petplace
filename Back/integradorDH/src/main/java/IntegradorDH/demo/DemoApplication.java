cc8ac1c876e3ddd1c08d38815c21188eb22cf7ef
JeiferJC <Jeifer.as@gmail.com>
2023-05-12 10:19:02 -0500

    segundo commit prueba

--- Back/integradorDH/src/main/java/IntegradorDH/demo/DemoApplication.java
+++ Back/integradorDH/src/main/java/IntegradorDH/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	private String hola2;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
