20555dd1ec51ff015b25c8a5e98518591a907d6c
lizz medina <lizzmedina15@gmail.com>
2023-05-12 22:01:27 -0500

    cambio de fondo blanco para la navbar y el home, se reemplazan imagenes de las cardCategories por una estandar para optimizar el rendimiento

--- Front/src/routes/Categories.jsx
+++ Front/src/routes/Categories.jsx
@@ -9,18 +9,21 @@ export const Categories = ({categories}) => {
   //const categories = useState['finca', 'casa De Familia', 'guardería multiple' ];
   const [places, setPlaaces] = useState([
     {
+      id: 1,
       category: 'finca',
-      image: './images/finca1.jpg',
+      image: './images/imagenpiloto.png',
       quantity: 10,
     },
     {
+      id: 2,
       category: 'casa De Familia',
-      image: './images/casa1.jpg',
+      image: './images/imagenpiloto.png',
       quantity: 20,
     },
     {
+      id: 3,
       category: 'guardería multiple',
-      image: './images/guarderia1.jpg',
+      image: './images/imagenpiloto.png',
       quantity: 5,
     }
   ])
@@ -31,7 +34,7 @@ export const Categories = ({categories}) => {
     <div className="render-cards-categories">
       {places.map(place => (
         <Card 
-          key={place.image}
+          key={place.id}
           category={place.category}
           image={place.image}
           quantity={place.quantity}
