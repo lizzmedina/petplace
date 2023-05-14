2cd3e12568a2a122f36a84e38eb206be6dcdaa9e
VivianaGuzmanBuritica <82985780+VivianaGuzmanBuritica@users.noreply.github.com>
2023-05-14 13:42:41 -0500

    creacion funcionalidad booking con controller, pendiente relacion tablas para brobar esta funcionalidad

--- Back/IntegradorMaven/src/main/java/com/example/demo/controller/BookingController.java
+++ Back/IntegradorMaven/src/main/java/com/example/demo/controller/BookingController.java
@@ -1,5 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.DTO.BookingDTO;
+import com.example.demo.entity.Booking;
+import com.example.demo.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 // ANDREA VIVI
+@RestController
+@RequestMapping(value = "/api/v1/booking")
 public class BookingController {
+
+    private BookingService service;
+
+    @Autowired
+    public BookingController(BookingService service) {
+        this.service = service;
+    }
+
+    @PostMapping()
+    public Booking save (@RequestBody BookingDTO bookingDTO){
+        return service.save(bookingDTO);
+    }
+
+    @GetMapping("/{id}")
+    public Booking detail (@PathVariable("id") Integer id){
+        return service.detail(id);
+    }
 }
--- Back/IntegradorMaven/src/main/java/com/example/demo/service/BookingService.java
+++ Back/IntegradorMaven/src/main/java/com/example/demo/service/BookingService.java
@@ -1,5 +1,81 @@
 package com.example.demo.service;
 
+import com.example.demo.DTO.BookingDTO;
+import com.example.demo.entity.Booking;
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.PetDayCare;
+import com.example.demo.repository.BookingRepository;
+import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.PetDayCareRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 //andrea vivi
 public class BookingService {
+
+    private List<Booking> bookingList;
+    private BookingRepository bookingRepository;
+    private CustomerRepository customerRepository;
+    private PetDayCareRepository petDayCareRepository;
+
+    public BookingService(List<Booking> bookingList, BookingRepository bookingRepository, CustomerRepository customerRepository, PetDayCareRepository petDayCareRepository) {
+        this.bookingList = bookingList;
+        this.bookingRepository = bookingRepository;
+        this.customerRepository = customerRepository;
+        this.petDayCareRepository = petDayCareRepository;
+    }
+
+    public Booking save(BookingDTO bookingDTO){
+
+        Optional<Customer> customer = this.customerRepository.findById(bookingDTO.getCustomerId());
+        Optional<PetDayCare> petDayCare = this.petDayCareRepository.findById(bookingDTO.getPetDayCareId());
+
+        if(!customer.isPresent() && !petDayCare.isPresent()){
+            throw  new RuntimeException("Verifique que el cliente o la guarderia existan");
+        }
+
+        double totalpriceBooking = calculatePrice(bookingDTO.getCheckIn(), bookingDTO.getCheckOut(), petDayCare.get().getBasicPrice());
+
+        Booking newBooking = new Booking(
+                totalpriceBooking,
+                bookingDTO.getCheckIn(),
+                bookingDTO.getCheckOut()
+             //   customer,
+             //   petDayCare
+        );
+
+        return bookingRepository.save(newBooking);
+    }
+
+
+    public double calculatePrice(Date checkIn, Date checkOut, double basicPrice){
+
+        long startTime = checkIn.getTime() ;
+        long endTime = checkOut.getTime();
+        long daysSince = (long) Math.floor(startTime / (1000*60*60*24)); // convert to days, so that time changes do not affect
+        long daysUntil = (long) Math.floor(endTime / (1000*60*60*24)); // convert to days, so that time changes do not affect
+        long totalDays = daysUntil-daysSince;
+
+                double total = (totalDays * basicPrice);
+
+        return total;
+    }
+
+    public Booking detail(Integer id){
+        Optional<Booking> booking = bookingRepository.findById(id);
+
+        if(!booking.isPresent()){
+            throw new RuntimeException("la reserva no existe, verifique el numero de id");
+        }
+
+        Booking bookingDetail = new Booking(
+                booking.get().getTotalPrice(),
+                booking.get().getCheckIn(),
+                booking.get().getCheckOut()
+        );
+
+        return bookingDetail;
+    }
 }
