bb14dfaf2485ad51cb42fd9f0c8f45fcd5dc4679
yessid_vargas <yevasa93@gmail.com>
2023-05-15 18:30:44 -0500

    modificaciones en CardCategories para renderizar segun categoria

--- Front/src/routes/CategoryFinca.jsx
+++ Front/src/routes/CategoryFinca.jsx
@@ -6,27 +6,6 @@ const CategoryFinca = () => {
     const {dataCategory, setDataCategory, items, setItems, currentPage, setCurrentPage,
         prevHandler, nextHandler, startHandler, endHandler} = useContextGlobal()
 
-    // ++++++++++++++++++++++++++++++++++++++
-    //esta sera la logica para filtrar los productos bajo esta categoria y finalizar en un setDataCategory
-
-    // ------Nota el feth se deberia hacer en context y luego aqui en componente 
-    // filtrarlo para luego meter el array en el use effect
-
-    // useEffect(() => {
-    //     const datosFiltradosAPI = dataCategory.filter(producto => producto.type === 'Finca')
-    //     setDataCategory(datosFiltradosAPI)
-    // }, [])
-    const datosFiltradosAPI = Array.from({length:40}, (value, index) => {
-        return {id: index, name: `Item #${index}`}
-    })
-    
-    useEffect(()=>{     
-        setDataCategory(datosFiltradosAPI)
-    }, [])
-    
-    // ++++++++++++++++++++++++++++++++++++++++++
-
-
     return (
         <div className='space-section'>
             <h2>Finca</h2>
