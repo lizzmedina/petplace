f563e200999062e6e889d1add99ff3f0c31c7f9c
JorgePreciadoG <jhpreciadog@gmail.com>
2023-05-15 20:31:04 -0500

    arreglo de logo

--- Front/src/components/Searcher.jsx
+++ Front/src/components/Searcher.jsx
@@ -12,30 +12,12 @@ export const Searcher = () => {
       
     <div className="searchercontainer2">
         <div className="searcherLogo-container">
-          <img 
-            src='./images/PpLogo.png'  
-            alt="icono"
-            className="searcherLogo"
-          />
+          
       </div>
       <div className="searcher">
-        <div className="search-by-location">
-          <label htmlFor="opciones"/>
-          <select id="opciones" value={opcionSeleccionada} onChange={handleSeleccionarOpcion}>
-            <option value="" disabled hidden>¿dónde estarás? </option>
-            {opciones.map((opcion) => (
-              <option key={opcion} value={opcion}>
-                {opcion}
-              </option>
-            ))}
-          </select>
-        </div>
-      <input 
-        className = "searcher-input-date"
-        type = "text"
-        placeholder = "CheckIn - CheckOut"
-      />
-    <button className="searcher-button">Buscar</button>
+        <SearcherByLocation/>
+        <ReservationCalendar/>
+        <button className="searcher-button">Buscar</button>
       </div>
     </div>
       
