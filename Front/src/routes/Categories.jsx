4a5193409b6a7d1bcc3848812d712b87df80955a
lizz medina <lizzmedina15@gmail.com>
2023-05-17 20:10:24 -0500

    actualizar imagenes de categorias

--- Front/src/routes/Categories.jsx
+++ Front/src/routes/Categories.jsx
@@ -7,25 +7,25 @@ export const Categories = ({categories}) => {
     {
       id: 1,
       category: 'Perros',
-      image: './images/hermoso-labrador.avif',
+      image: 'https://img.freepik.com/free-psd/group-portrait-adorable-puppies_53876-73962.jpg?w=1380&t=st=1684284771~exp=1684285371~hmac=112f48515e6ce16541da2e8d02b6e4d921e0e4b03d7a16778606b1aae672e3a6',
       quantity: '20 Fincas',
     },
     {
       id: 2,
       category: 'Gatos',
-      image: './images/hermoso-labrador.avif',
+      image: 'https://img.freepik.com/premium-photo/birman-cats-looking-camera_191971-18018.jpg?w=900',
       quantity: '20 Casas',
     },
     {
       id: 3,
       category: 'Canarios',
-      image: './images/hermoso-labrador.avif',
+      image: 'https://img.freepik.com/free-photo/cape-may-warbler_181624-6537.jpg?w=1060&t=st=1684283666~exp=1684284266~hmac=4954a4d4ea1ea030ce2203f072f4002700b8c9307719df86cf89c8671a902f9c',
       quantity: '20 Guarderias',
     },
     {
       id: 4,
       category: 'Conejos',
-      image: './images/hermoso-labrador.avif',
+      image: 'https://img.freepik.com/free-photo/furry-cute-rabbit-isolated_78492-3950.jpg?w=996&t=st=1684283969~exp=1684284569~hmac=7d171e3194b330b91771c73920494d0fcac6fd4c9926091e3ebda94fb6d5d3b4',
       quantity: '20 Guarderias',
     }
   ])
