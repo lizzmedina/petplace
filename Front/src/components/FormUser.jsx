c9f3b06cad6f7eb0ea6f4b3d02c7301a96920ef3
yessid_vargas <yevasa93@gmail.com>
2023-05-24 23:49:06 -0500

    ajustes en la validacion de usuario

--- Front/src/components/FormUser.jsx
+++ Front/src/components/FormUser.jsx
@@ -4,7 +4,7 @@ import * as yup from "yup";
 
 const FormUser = () => {
     const [user, setUser] = useState({
-        id: 0,
+        id: '',
         name:'',
         lastName:'',
         cellPhone:'',
@@ -15,9 +15,10 @@ const FormUser = () => {
     });
 
     const schema = yup.object().shape({
-        name: yup.string().required("El nombre es obligatorio").matches(/^[a-zA-Z]+$/, "El nombre no debe contener caracteres especiales, numeros ni espacios en blanco"),
-        lastName: yup.string().required("El apellido es obligatorio").matches(/^[a-zA-Z]+$/, "El nombre no debe contener caracteres especiales ni espacios en blanco"),
-        cellPhone: yup.string().required("El número de celular es obligatorio").min(5, "El número de celular debe tener al menos 5 caracteres"),
+        name: yup.string().required("El nombre es obligatorio").matches(/^[a-zA-Z]+(\s[a-zA-Z]+)*$/, "El nombre no debe contener caracteres especiales, números ni espacios en blanco al inicio o final"),
+        lastName: yup.string().required("El apellido es obligatorio").matches(/^[a-zA-Z]+(\s[a-zA-Z]+)*$/, "El apellido no debe contener caracteres especiales ni espacios en blanco al inicio o final"),
+        id: yup.string().required("El Documento de Identidad es obligatorio").matches(/^[0-9]+$/, "El Documento de identidad solo debe contener números sin puntos ni comas").min(5, "El Documento de identidad debe tener al menos 5 caracteres"),
+        cellPhone: yup.string().required("El Celular es obligatorio").matches(/^[0-9]+$/, "El Celular solo debe contener números sin puntos ni comas").min(5, "El número de celular debe tener al menos 5 caracteres"),
         address: yup.string().required("La dirección es obligatoria"),
         email: yup.string().email("El email no es válido").required("El email es obligatorio").matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "El email no debe contener caracteres especiales, espacios en blanco y seguir un formato xxx@xxx.xxx"),
         password: yup.string().required("La contraseña es obligatoria"),
@@ -45,20 +46,9 @@ const FormUser = () => {
             await schema.validate(user, { abortEarly: false });
             // La validación es exitosa con Yup, se continúa con el resto
 
-            const result = allUsers.find((item) => item.email === user.email);
-            if (result === undefined) {
+            const result = allUsers.find((item) => item.email.toLowerCase() === user.email.toLowerCase());
+            if (!result) {
                 // El email no existe, proceder con el envío del formulario
-                setUser({
-                    id: 0,
-                    name: "",
-                    lastName: "",
-                    cellPhone: "",
-                    address: "",
-                    email: "",
-                    password: "",
-                    type: "",
-                });
-
                 setIsLoading(true);
 
                 fetch("http://127.0.0.1:8080/api/v1/user", {
@@ -73,14 +63,14 @@ const FormUser = () => {
                             setIsSuccess(true);
                             alert(`El Usuario ${user.name} ha sido creado exitosamente.`);
                             setUser({
-                                id: 0,
-                                name: "",
-                                lastName: "",
-                                cellPhone: "",
-                                address: "",
-                                email: "",
-                                password: "",
-                                type: "",
+                                id: '',
+                                name:'',
+                                lastName:'',
+                                cellPhone:'',
+                                address:'',
+                                email:'',
+                                password:'',
+                                type:''
                             });
                         } else {
                             throw new Error("Error en la solicitud");
@@ -92,6 +82,7 @@ const FormUser = () => {
                     })
                     .finally(() => {
                         setIsLoading(false);
+                        setUser((user) => ({ ...user, type: "" })); // Restablecer el campo "type" a una cadena vacía
                     });
             } else {
                 alert(
@@ -121,16 +112,27 @@ const FormUser = () => {
                         <br/>
                     </div>
                     <div className="box">
-                        <label>Apellido: </label><br/>
+                        <label>Apellidos: </label><br/>
                         <input type="text" value={user.lastName} onChange={(e) => setUser({...user, lastName: e.target.value})}/>
                         {validationErrors.lastName && (<span className="error-message">{validationErrors.lastName}</span>)}
                         <br/>
                     </div>
                 </div>
                 <br/>
-                <label>Celular: </label>
-                <input type="text" value={user.cellPhone} onChange={(e) => setUser({...user, cellPhone: e.target.value})}/>
-                {validationErrors.cellPhone && (<span className="error-message">{validationErrors.cellPhone}</span>)}
+                <div className="form-section-name">
+                    <div className="box">
+                        <label>Documento de Identidad: </label><br/>
+                        <input type="number" value={user.id} onChange={(e) => setUser({...user, id: e.target.value})}/>
+                        {validationErrors.id && (<span className="error-message">{validationErrors.id}</span>)}
+                        <br/>
+                    </div>
+                    <div className="box">
+                        <label>Celular: </label><br/>
+                        <input type="number" value={user.cellPhone} onChange={(e) => setUser({...user, cellPhone: e.target.value})}/>
+                        {validationErrors.cellPhone && (<span className="error-message">{validationErrors.cellPhone}</span>)}
+                        <br/>
+                    </div>
+                </div>
                 <br/>
                 <label>Dirección: </label>
                 <input type="text" value={user.address} onChange={(e) => setUser({...user, address: e.target.value})}/>
@@ -145,8 +147,8 @@ const FormUser = () => {
                 {validationErrors.password && (<span className="error-message">{validationErrors.password}</span>)}
                 <br/>
                 <label>Tipo de Usuario: </label>
-                <select name="type" onChange={(e) => setUser({...user, type: e.target.value})}>
-                    <option selected hidden>--- Elige una Opcion ---</option>
+                <select name="type" value={user.type} onChange={(e) => setUser({...user, type: e.target.value})}>
+                    <option value="" hidden>--- Elige una Opción ---</option>
                     <option value="customer">Cliente</option>
                     <option value="manager">Administrador</option>
                 </select>
