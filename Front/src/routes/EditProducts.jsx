fe0d766341e185f28006e4eec8fee1ee705907df
yessid_vargas <yevasa93@gmail.com>
2023-05-30 07:09:21 -0500

    form para editar

--- Front/src/routes/EditProducts.jsx
+++ Front/src/routes/EditProducts.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useState, useEffect } from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useContextGlobal } from '../components/utils/global.constext'
+import { useContextGlobal } from '../components/utils/global.constext';
 import Swal from 'sweetalert2';
 
 function EditProducts() {
-    const {places, setPlaces} = useContextGlobal()
+    const { places, setPlaces } = useContextGlobal();
 
     const [allProducts, setAllProducts] = useState([]);
-    const getAllProducts = async()=> {
+    const getAllProducts = async () => {
         const res = await fetch('http://localhost:8080/api/v1/petDayCare/all');
         const data = await res.json();
-        setAllProducts(data)
-    }
+        setAllProducts(data);
+    };
     useEffect(() => {
         getAllProducts();
     }, []);
@@ -25,19 +25,22 @@ function EditProducts() {
             });
             getAllProducts();
         }
-    }
-
+    };
 
     return (
         <div className='space-section'>
             <h2>Lista de Productos</h2>
             <div className='ListItem-CreateProduct'>
-                <button className='create-button'> <Link to='/productRegister' style={{ color: 'inherit' }}>Crear Producto</Link> </button>
+                <button className='create-button'>
+                    {' '}
+                    <Link to='/productRegister' style={{ color: 'inherit' }}>
+                        Crear Producto
+                    </Link>{' '}
+                </button>
             </div>
             {allProducts.map((product) => (
-                <div className='ListItem'>   {/*El map mostrara 2 cosas: el nombre y los botones al final*/}
+                <div className='ListItem'>
                     {product.name}
-
                     <div className='ListButtons'>
                         <button
                             className='edit-button'
@@ -50,52 +53,91 @@ function EditProducts() {
                                         '<p>Capacidad: </p> ' +
                                         `<input id="capacidad" class="swal2-input" value="${product.capacity}" required>` +
                                         '<p>Precio: </p> ' +
-                                        `<input id="precio" class="swal2-input" value="${product.basic_price}" required>` +
+                                        `<input id="precio" class="swal2-input" value="${product.basicPrice}" required>` +
                                         '<p>Categoria: </p> ' +
-                                        `<select name="type" value="${product.type.title}">
-                                            ${places.map((categoria) => (
-                                                `<option value="${categoria.title}">${categoria.title}</option>`
-                                            ))}
+                                        `<select id="categoria" class="swal2-input">
+                                        ${places.map(
+                                            (categoria) =>
+                                                `<option value="${categoria.title}" ${categoria.title === product.type.title ? 'selected' : ''
+                                                }>${categoria.title}</option>`
+                                        )}
                                         </select>` +
-
                                         '<p>Ciudad: </p> ' +
                                         `<input id="ciudad" class="swal2-input" value="${product.city}" required>` +
                                         '<p>Direccion: </p> ' +
                                         `<input id="direccion" class="swal2-input" value="${product.address}" required>` +
                                         '<p>Descripcion: </p> ' +
-                                        `<input id="descripcion" class="swal2-input" value="${product.detail}" required>`,
+                                        `<input id="detail" class="swal2-input" value="${product.detail}" required>`,
                                     focusConfirm: false,
                                     showCancelButton: true,
                                     preConfirm: () => {
                                         const nombre = document.getElementById('nombre').value;
-                                        const descripcion = document.getElementById('descripcion').value;
-                                        if (!nombre || !descripcion) {
+                                        const capacidad = document.getElementById('capacidad').value;
+                                        const precio = document.getElementById('precio').value;
+                                        const categoria = document.getElementById('categoria').value;
+                                        const ciudad = document.getElementById('ciudad').value;
+                                        const direccion = document.getElementById('direccion').value;
+                                        const detail = document.getElementById('detail').value;
+
+                                        if (!nombre || !capacidad || !precio || !categoria || !ciudad || !direccion || !detail) {
                                             Swal.showValidationMessage('Debes completar todos los campos');
                                         }
-                                        return { nombre, descripcion };
+
+                                        return { nombre, capacidad, precio, categoria, ciudad, direccion, detail };
                                     },
                                     customClass: {
                                         container: 'my-swal-container',
                                         popup: 'my-swal-popup',
                                     },
                                 }).then((result) => {
-                                    if (!result.dismiss) {
-                                        const { nombre, descripcion } = result.value;
-                                        Swal.fire(`Nuevo nombre: ${nombre}, Nueva descripción: ${descripcion}`);
-                                    }
-                                });
 
+
+                                    const { nombre, capacidad, precio, categoria, ciudad, direccion, detail } = result.value;
+                                    console.log(categoria);
+                                    console.log(detail);
+
+                                    const updatedProduct = {
+                                        id: product.id,
+                                        name: nombre,
+                                        capacity: capacidad,
+                                        basicPrice: precio,
+                                        categoryName: "Gatos",
+                                        city: ciudad,
+                                        address: direccion,
+                                        detail: detail,
+                                        images: product.images,
+                                        characteristics: product.characteristics,
+                                    };
+
+                                    fetch('http://127.0.0.1:8080/api/v1/petDayCare/edit', {
+                                        method: 'PUT',
+                                        headers: {
+                                            'Content-Type': 'application/json',
+                                        },
+                                        body: JSON.stringify(updatedProduct),
+                                    })
+                                        .then((response) => response.json())
+                                        .then((data) => {
+                                            Swal.fire(`Producto actualizado: ${data.name}`);
+                                            getAllProducts(); // Actualiza la lista de productos después de la edición
+                                        })
+                                        .catch((error) => {
+                                            console.error('Error:', error);
+                                        });
+                                });
                             }}
                         >
                             Editar
                         </button>
 
-                        <button className='delete-button' onClick={() => deleteProduct(product.id, product.name)}> Eliminar </button>
+                        <button className='delete-button' onClick={() => deleteProduct(product.id, product.name)}>
+                            Eliminar
+                        </button>
                     </div>
                 </div>
             ))}
         </div>
-    )
+    );
 }
 
-export default EditProducts
+export default EditProducts;
