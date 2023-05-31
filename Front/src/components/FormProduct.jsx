232dbe5ba5412c2a8623c8dd72b4d84802f1916c
yessid_vargas <yevasa93@gmail.com>
2023-05-30 23:02:31 -0500

    mas imagenes en producto

--- Front/src/components/FormProduct.jsx
+++ Front/src/components/FormProduct.jsx
@@ -46,7 +46,7 @@ const FormProduct = () => {
                     images: [],
                     characteristics: [],
                     basicPrice: "",
-                });
+                });console.log(product)
 
                 fetch("http://127.0.0.1:8080/api/v1/petDayCare", {
                     method: "POST",
@@ -54,6 +54,7 @@ const FormProduct = () => {
                         "Content-Type": "application/json",
                     },
                     body: JSON.stringify(product),
+
                 })
                     .then((response) => {
                         if (response.ok) {
@@ -63,7 +64,7 @@ const FormProduct = () => {
                             );
                             setProduct({
                                 name: "",
-                                categoryName: "",
+                                categoryName: "", // Agregar esta línea
                                 capacity: "",
                                 city: "",
                                 address: "",
@@ -72,6 +73,8 @@ const FormProduct = () => {
                                 characteristics: [],
                                 basicPrice: "",
                             });
+                            const selectElement = document.querySelector("select[name='type']");
+                            selectElement.selectedIndex = 0;
                         } else {
                             throw new Error("Error en la solicitud");
                         }
@@ -82,6 +85,7 @@ const FormProduct = () => {
                     })
                     .finally(() => {
                         setIsLoading(false);
+                        setProduct((product) => ({ ...product, categoryName: "" }));
                     });
             } else {
                 alert("Parece que algo salió mal, recuerda llenar los campos.");
@@ -111,105 +115,51 @@ const FormProduct = () => {
                     <div className="box">
                         <label>Nombre: </label>
                         <br />
-                        <input
-                            type="text"
-                            value={product.name}
-                            onChange={(e) =>
-                                setProduct({ ...product, name: e.target.value })
-                            }
-                        />
+                        <input type="text" value={product.name} onChange={(e) => setProduct({ ...product, name: e.target.value })} />
                         <br />
                     </div>
                     <div className="box">
                         <label>Capacidad: </label>
                         <br />
-                        <input
-                            type="text"
-                            value={product.capacity}
-                            onChange={(e) =>
-                                setProduct({ ...product, capacity: e.target.value })
-                            }
-                        />
-                        <br />
-                    </div>
-                    <div className="box">
-                        <label>Precio: </label>
-                        <br />
-                        <input
-                            type="text"
-                            value={product.basicPrice}
-                            onChange={(e) =>
-                                setProduct({ ...product, basicPrice: e.target.value })
-                            }
-                        />
+                        <input type="text" value={product.capacity} onChange={(e) => setProduct({ ...product, capacity: e.target.value })} />
                         <br />
                     </div>
                 </div>
                 <br />
-                <div className="form-section-name">
-                    <div className="box">
-                        <label>Imagen: </label>
-                        <br />
-                        <input
-                            type="text"
-                            value={product.images[0]}
-                            onChange={(e) => handleImageChange(0, e.target.value)}
-                        />
-                        <button type="button" onClick={handleAddImage}>
-                            Agregar Imagen
-                        </button>
+
+
+                <div className="section-Image">
+                    <label>Agregar Imagen: </label> <button type="button" onClick={handleAddImage} className="button-AddImage"> + </button>
+                </div>
+                {product.images.map((imageUrl, index) => (
+                    <div>
+                        <input type="text" value={imageUrl} onChange={(e) => handleImageChange(index, e.target.value)} 
+                        className="input-Image" placeholder={"Imagen" + (index + 1)}/>
                         <br />
                     </div>
-                    {product.images.slice(1).map((imageUrl, index) => (
-                        <div className="box" key={index + 1}>
-                            <label>Imagen {index + 2}: </label>
-                            <br />
-                            <input
-                                type="text"
-                                value={imageUrl}
-                                onChange={(e) => handleImageChange(index + 1, e.target.value)}
-                            />
-                            <br />
-                        </div>
-                    ))}
-                </div>
+                ))}
+
+
                 <br />
                 <label>Tipo de alojamiento: </label>
-                <select
-                    name="type"
-                    onChange={(e) =>
-                        setProduct({ ...product, categoryName: e.target.value })
-                    }
-                >
+                <select name="type" onChange={(e) => setProduct({ ...product, categoryName: e.target.options[e.target.selectedIndex].value })}>
+                <option value="" hidden>--- Elige una Opción ---</option>
                     {places.map((categoria) => (
-                        <option key={categoria.title} value={categoria.title}>
-                            {categoria.title}
-                        </option>
+                        <option key={categoria.title} value={categoria.title}> {categoria.title} </option>
                     ))}
                 </select>
                 <br />
                 <label>Ciudad: </label>
-                <input
-                    type="text"
-                    value={product.city}
-                    onChange={(e) => setProduct({ ...product, city: e.target.value })}
-                />
+                <input type="text" value={product.city} onChange={(e) => setProduct({ ...product, city: e.target.value })} />
                 <br />
                 <label>Direccion: </label>
-                <input
-                    type="text"
-                    value={product.address}
-                    onChange={(e) => setProduct({ ...product, address: e.target.value })}
-                />
+                <input type="text" value={product.address} onChange={(e) => setProduct({ ...product, address: e.target.value })} />
+                <br />
+                <label>Precio: </label>
+                <input type="text" value={product.basicPrice} onChange={(e) => setProduct({ ...product, basicPrice: e.target.value })}/>
                 <br />
                 <label>Descripción: </label>
-                <textarea
-                    name="message"
-                    type="text"
-                    rows="5"
-                    value={product.detail}
-                    onChange={(e) => setProduct({ ...product, detail: e.target.value })}
-                />
+                <textarea name="message" type="text" rows="5" value={product.detail} onChange={(e) => setProduct({ ...product, detail: e.target.value })} />
                 <br />
                 <br />
                 <div className="section-button">
