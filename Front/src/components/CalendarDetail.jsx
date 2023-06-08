3ef080073d2f8ddc4ea1ad9da948292fabb68e8a
lizz medina <lizzmedina15@gmail.com>
2023-06-08 15:54:04 -0500

    calendar. ahora puede pasar meses

--- Front/src/components/CalendarDetail.jsx
+++ Front/src/components/CalendarDetail.jsx
@@ -1,48 +1,81 @@
-
-import dayjs from 'dayjs';
-import AdapterDayjs from '@mui/lab/AdapterDayjs'; 
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-
+import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export const CalendarDetail = () => {
-    const currentDate = dayjs();
-
     const datesData = [
-        ['2023-06-08', '2023-06-10'],
-        ['2023-07-23', '2023-07-26'],
-        ['2023-07-02', '2024-06-08'],
-        ['2023-06-12', '2024-06-13'],
+        ["2023-06-12", "2023-06-15"],
+        ["2023-07-23", "2023-07-26"],
+        ["2023-07-10", "2023-07-15"],
+        ["2023-06-19", "2023-06-21"],
     ];
 
-    const startDate = currentDate.startOf('month').format('YYYY-MM-DD');
-    const endDate = currentDate.endOf('month').format('YYYY-MM-DD');
+    const [dates, setDates] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
 
-    const renderDay = (day, _props) => {
-        return (
-        <div>
-            {dayjs(day).format('D')}
-        </div>
-        );
+    useEffect(() => {
+        const markedMonths = datesData.map(([startDate]) => new Date(startDate).getMonth());
+        if (markedMonths.length > 0) {
+        const minMonth = Math.min(...markedMonths);
+        setCurrentMonth(minMonth);
+        }
+    }, []);
+
+    const handleDateChange = (date) => {
+        setDates(date);
     };
 
-    const shouldDisableDate = (date) => {
-        for (const [start, end] of datesData) {
-        if (date >= start && date <= end) {
+    const tileDisabled = ({ date }) => {
+        for (const [startDate, endDate] of datesData) {
+        const currentDate = new Date(date);
+        if (currentDate >= new Date(startDate) && currentDate <= new Date(endDate)) {
             return true;
         }
         }
         return false;
     };
 
+    const handlePrevMonth = () => {
+        setCurrentMonth((prevMonth) => prevMonth - 1);
+    };
+
+    const handleNextMonth = () => {
+        setCurrentMonth((prevMonth) => prevMonth + 1);
+    };
+
     return (
-        <div>
-        <h1>Calendario</h1>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <CalendarPicker
-                renderDay={renderDay}
-                shouldDisableDate={(date) => date < startDate || date > endDate || shouldDisableDate(date)}
+        <div className="calendar-section-container">
+        <h2>Calendario</h2>
+        <div className="calendars-render">
+            <div className="calendar">
+            <button className="calendar-navigation-button" onClick={handlePrevMonth}>
+                <ChevronLeftIcon />
+            </button>
+            <Calendar
+                value={dates}
+                onChange={handleDateChange}
+                tileDisabled={tileDisabled}
+                calendarType="ISO 8601"
+                minDetail="month"
+                activeStartDate={new Date(new Date().getFullYear(), currentMonth, 1)}
             />
-        </LocalizationProvider>
+            </div>
+            <div className="calendar">
+            <button className="calendar-navigation-button" onClick={handleNextMonth}>
+                <ChevronRightIcon />
+            </button>
+            <Calendar
+                value={dates}
+                onChange={handleDateChange}
+                tileDisabled={tileDisabled}
+                calendarType="ISO 8601"
+                minDetail="month"
+                activeStartDate={new Date(new Date().getFullYear(), currentMonth + 1, 1)}
+            />
+            </div>
+        </div>
         </div>
     );
 };
