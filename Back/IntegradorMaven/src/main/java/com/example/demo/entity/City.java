9ce8a2256bd9c85e0d0789a30f66e32c53fbf216
Maria <mariugiraldo40@gmail.com>
2023-06-11 17:01:32 -0500

    nombre de ciudad con restriccion de unicidad en la BD

--- Back/IntegradorMaven/src/main/java/com/example/demo/entity/City.java
+++ Back/IntegradorMaven/src/main/java/com/example/demo/entity/City.java
@@ -14,7 +14,7 @@ public class City {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column
+    @Column(unique = true)
     private String name;
 
     @JsonIgnore
