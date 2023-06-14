09c565badece5c5fa2dc50d7d37f1be087bf8aa5
Your Name <you@example.com>
2023-06-13 19:32:22 -0500

    Subiendo Booking y users a la BD, corrigiendo el put y post del petdaycare, agregdo fidById en Booking

--- Back/IntegradorMaven/src/main/java/com/example/demo/service/CityService.java
+++ Back/IntegradorMaven/src/main/java/com/example/demo/service/CityService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -30,6 +31,7 @@ public class CityService {
     public CityDTO findById(Integer id) {
         return cityMapper.mapToDto(cityRepository.findById(id).get());
     }
+
 
     public CityDTO findByName(String name){
         return cityMapper.mapToDto(cityRepository.findByName(name).get());
