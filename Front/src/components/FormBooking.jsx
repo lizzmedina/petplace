580d5376b8932192a8d5d643c6bf0d9232afe6d1
yessid_vargas <yevasa93@gmail.com>
2023-06-24 18:28:52 -0500

    ajuste en componente rangePicker

--- Front/src/components/FormBooking.jsx
+++ Front/src/components/FormBooking.jsx
@@ -3,11 +3,15 @@ import { useLocation } from 'react-router-dom';
 import { useNavigate, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faShower, faPersonWalkingWithCane, faCarrot, faBaseball, faStethoscope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { ReservationCalendar } from "./ReservationCalendar";
 import { Link as ScrollLink, Element } from 'react-scroll';
 import ImageModal from './ImageModal';
 import { CalendarDetail } from "./CalendarDetail.jsx";
+import { useContextGlobal } from './utils/global.constext';
 
 function FormBooking() {
+
+    const {selectedDates, setSelectedDates} = useContextGlobal();
 
     const user = JSON.parse(localStorage.getItem("userConnected"));
     const product = JSON.parse(sessionStorage.getItem("productDetail"));
@@ -71,17 +75,35 @@ function FormBooking() {
         setModalOpen(false);
     };
 
+    //Calculo del numero de dias
+    const startDate = new Date(selectedDates[0]);
+    const endDate = new Date(selectedDates[1]);
+
+    const timeDifference = endDate.getTime() - startDate.getTime();
+    const numberofDays = Math.floor(timeDifference / (1000 * 3600 * 24));
+
+    const totalPayment = product.basicPrice * numberofDays
+
 
     return (
         <div>
             <div className="booking-upcontainer">
                 <div className="booking-upleftcontainer">
-                    <h3>Información de Usuario</h3><br/>
-                    <div className='form-infoLine'> <p>id:&nbsp;{user.id}</p> </div>
-                    <div className='form-infoLine'> <p>Nombre(s):</p> <p>&nbsp;{user.name}</p></div>
-                    <div className='form-infoLine'> <p>Apellido(s):</p> <p>&nbsp;{user.lastName}</p> </div>
-                    <div className='form-infoLine'> <p>email:</p> <p>&nbsp;{user.email}</p> </div>
-                    <div className='form-infoLine'> <p>Celular:</p> <p>&nbsp;{user.cellPhone}</p> </div>
+                    <div className="booking-upleftsection">
+                        <h3>Información de Usuario</h3><br/>
+                        <div className='form-infoLine'> <p>id:&nbsp;{user.id}</p> </div>
+                        <div className='form-infoLine'> <p>Nombre(s):</p> <p>&nbsp;{user.name}</p></div>
+                        <div className='form-infoLine'> <p>Apellido(s):</p> <p>&nbsp;{user.lastName}</p> </div>
+                        <div className='form-infoLine'> <p>email:</p> <p>&nbsp;{user.email}</p> </div>
+                        <div className='form-infoLine'> <p>Celular:</p> <p>&nbsp;{user.cellPhone}</p> </div>
+                    </div>
+                    <div className="booking-upleftsection">
+                        <h3>Información de Reserva</h3><br/>
+                        <ReservationCalendar /><br/>
+                        <div className='form-infoLine'> <p>Precio base x dia(s):</p> <p>&nbsp;${product.basicPrice}</p> </div>
+                        <div className='form-infoLine'> <p>Dia(s):</p> <p>&nbsp; {numberofDays}</p> </div>
+                        <div className='form-infoLine'> <p>Total a Pagar:</p> <p>&nbsp; {totalPayment}</p> </div>
+                    </div>
                 </div>
 
                 <div className="booking-uprigthcontainer">
