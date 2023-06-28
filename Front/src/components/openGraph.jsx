8931562f4b557339eb38b49ccb8867b82463a3ad
JeiferJC <Jeifer.as@gmail.com>
2023-06-27 20:33:44 -0500

    se modifico el archivo openGraph

--- Front/src/components/openGraph.jsx
+++ Front/src/components/openGraph.jsx
@@ -13,18 +13,12 @@ const openGraph = () => {
   
     {/* <!-- Open Graph / Facebook --> */}
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="http://localhost:5173/" />
+    <meta property="og:url" content="http://bucket-equipo2-frontend.s3-website.us-east-2.amazonaws.com/" />
     <meta property="og:title" content="PetPlace Lo mejor para tu mascota!!" />
     <meta property="og:description" content="Hey!! revisa este excelente lugar de petPlace para que puedas dejar tu mascota a salvo y puedas viajar tranquilo" />
-    <meta property="og:image" content="/Detail/:id/{image[0]}" />
+    <meta property="og:image" content="https://bit.ly/3Nt4iOi" />
   
-   {/*  <!-- Twitter --> */}
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="http://localhost:5173/" />
-    <meta property="twitter:title" content="PetPlace Lo mejor para tu mascota!!" />
-    <meta property="twitter:description"
-      content="Hey!! revisa este excelente lugar de petPlace para que puedas dejar tu mascota a salvo y puedas viajar tranquilo" />
-    <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
   
     {/* <!-- Meta Tags Generated with https://metatags.io --> */}
     </div>
