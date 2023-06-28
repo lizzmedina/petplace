5cd7a09be0f19544cc339804de07016d93cb9b07
lizz medina <lizzmedina15@gmail.com>
2023-06-27 22:54:06 -0500

    listar favoritos y reservas con mensaje sino hay

--- Front/src/routes/Favorites.jsx
+++ Front/src/routes/Favorites.jsx
@@ -4,53 +4,63 @@ import { useContextGlobal } from "../components/utils/global.constext";
 
 export const Favorites = () => {
     const [favorites, setFavorites] = useState([]);
-    const {urlFavorites} = useContextGlobal;
-
-    useEffect(() => {
-      // Obtener el ID del usuario del localStorage
+    const {urlFavorites} = useContextGlobal();
+    
+    const fetchFavorites = () => {
         const userConnected = JSON.parse(localStorage.getItem("userConnected"));
-        const userId = userConnected.id;
-
-      // Realizar la solicitud GET con el ID del usuario
-    fetch(`${urlFavorites}${userId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          // Manejar la respuesta
-            setFavorites(data);
-        })
-        .catch((error) => {
-          // Manejar el error
-            console.error(error);
-        });
+        console.log(userConnected.id, 'funcion ');
+        if (userConnected) {
+            const userId = userConnected.id;
+            console.log(userId, 'if');
+            fetch(`${urlFavorites}${userId}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setFavorites(data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    };
+    useEffect(() => {
+        fetchFavorites();
     }, []);
 
+//   <h3 className="h3-no-favorites">Aún no se han agregado hospedajes a favoritos.</h3>;
+    console.log(favorites);
     return (
         <div className="space-section favorites-container">
             <h2>Favorites</h2>
+            
             {favorites.length === 0 ? (
-            <h3>Aún no se han agregado hospedajes a favoritos.</h3>
+                <h3 className="h3-no-favorites">Aún no se han agregado hospedajes a favoritos.</h3>
             ) : (
-            <div className="render-cards-recommends">
-                {favorites.map((favorite) => {
-                const petDayCare = favorite.petDayCare;
-                return (
-                    <CardRecomends
-                    key={petDayCare.id}
-                    number={petDayCare.id}
-                    type={petDayCare.type.title}
-                    name={petDayCare.name}
-                    image={petDayCare.images}
-                    capacity={petDayCare.capacity}
-                    rating={petDayCare.average}
-                    city={petDayCare.city.name}
-                    address={petDayCare.address}
-                    detail={petDayCare.detail}
-                    basicPrice={petDayCare.basicPrice}
-                    characteristics={petDayCare.characteristics}
-                    />
-                );
-                })}
-            </div>
+                <div className="render-cards-recommends">
+                    {favorites.map((favorite) => {
+                        const petDayCare = favorite.petDayCare;
+                        return (
+                            <CardRecomends
+                                key={petDayCare.id}
+                                number={petDayCare.id}
+                                type={petDayCare.type.title}
+                                name={petDayCare.name}
+                                image={petDayCare.images}
+                                capacity={petDayCare.capacity}
+                                rating={petDayCare.average}
+                                city={petDayCare.city.name}
+                                address={petDayCare.address}
+                                detail={petDayCare.detail}
+                                basicPrice={petDayCare.basicPrice}
+                                characteristics={petDayCare.characteristics}
+                            />
+                        );
+                    })}
+                </div>
             )}
         </div>
     );
